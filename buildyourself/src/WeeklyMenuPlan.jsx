69e181673d846b8817f801c8fe7ad808fcc607d2
irinapukish <irynapukish68@gmail.com>
2024-10-29 12:40:34 +0100

    Add menu plan for weekly

--- buildyourself/src/WeeklyMenuPlan.jsx
+++ buildyourself/src/WeeklyMenuPlan.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
-const TestDay = {
+const DayOne = {
   data: "Poniedziałek, 22.11",
   meals: [
+    
+   
     {
     text:"Posiłek 1: 07:00-10:00",
       name: "Wrap z łososiem i burratą",
@@ -14,23 +16,273 @@ const TestDay = {
         carbohydrates_per_100g: 33,
       },
     },
-    
+    {
+      text:"Posiłek 2: 12:00-15:00",
+        name: "Stek wolowy z kaszą",
+        imgSrc: "images/foto_przepisy/obiad/stek_wolowy_z_kasza.png",
+        kcal: {
+          calories_per_100g: 777,
+          protein_per_100g: 56,
+          fat_per_100g: 18,
+          carbohydrates_per_100g: 100,
+        },
+      },
+      {
+        text:"Posiłek 3: 17:00-21:00",
+          name: "Chłodnik truskawkowy",
+          imgSrc: "images/foto_przepisy/kolacja/chlodnik_truskawkowy.png",
+          kcal: {
+            calories_per_100g: 439,
+            protein_per_100g: 26,
+            fat_per_100g: 12,
+            carbohydrates_per_100g: 65,
+          },
+        },
   ],
 };
 
-const testWeek = [TestDay];
+const DayTwo = {
+  data: "Wtorek, 23.11",
+  meals: [
+    {
+      text:"Posiłek 1: 07:00-10:00",
+      name: "Kanapki z serem i jajkiem sadzonym",
+      imgSrc: "images/foto_przepisy/sniadanie/kanapki_z_serem_i_jajkiem_sadzonym.png",
+      kcal: {
+        calories_per_100g: 732,
+        protein_per_100g: 35,
+        fat_per_100g: 37,
+        carbohydrates_per_100g: 72,
+      },
+    },
+    {
+      text:"Posiłek 2: 12:00-15:00",
+        name: "Łosoś z marchewką i ziemniakami",
+        imgSrc: "images/foto_przepisy/obiad/losos_z_marchewka_i_ziemniakami.png",
+        kcal: {
+          calories_per_100g: 472,
+          protein_per_100g: 30,
+          fat_per_100g: 23,
+          carbohydrates_per_100g: 41,
+        },
+      },
+      {
+        text:"Posiłek 3: 17:00-21:00",
+          name: "Klasyczne pancakes",
+          imgSrc: "images/foto_przepisy/kolacja/klasyczne_pancakes.png",
+          kcal: {
+            calories_per_100g: 192,
+            protein_per_100g: 5,
+            fat_per_100g: 10,
+            carbohydrates_per_100g: 22,
+          },
+        },
+  ],
+};
+
+const DayThree = {
+  data: "Środa, 24.11",
+  meals: [
+    {
+      text:"Posiłek 1: 07:00-10:00",
+      name: "Kanapki z focacci z prosciutto i mozzarellą",
+      imgSrc: "images/foto_przepisy/sniadanie/kanapki_z_focacci_z_prosciutto_i_mozzarella.png",
+      kcal: {
+        calories_per_100g: 406,
+        protein_per_100g: 22,
+        fat_per_100g: 16,
+        carbohydrates_per_100g: 42,
+      },
+    },
+    {
+      text:"Posiłek 2: 12:00-15:00",
+        name: "Pinsa z szynką i rukolą",
+        imgSrc: "images/foto_przepisy/obiad/pinsa_z_szynka_i_rukola.png",
+        kcal: {
+          calories_per_100g: 262,
+          protein_per_100g: 15,
+          fat_per_100g: 9,
+          carbohydrates_per_100g: 29,
+        },
+      },
+      {
+        text:"Posiłek 3: 17:00-21:00",
+          name: "Koktajl bananowy",
+          imgSrc: "images/foto_przepisy/kolacja/koktajl_bananowy.png",
+          kcal: {
+            calories_per_100g: 505,
+            protein_per_100g: 31,
+            fat_per_100g: 18,
+            carbohydrates_per_100g: 60,
+          },
+        },
+  ],
+};
+const DayFour = {
+  data: "Czwartek, 25.11",
+  meals: [
+    {
+      text:"Posiłek 1: 07:00-10:00",
+      name: "Francuskie paluchy z pesto",
+      imgSrc: "images/foto_przepisy/sniadanie/francuskie_paluchy_z_pesto.png",
+      kcal: {
+        calories_per_100g: 428,
+        protein_per_100g: 7,
+        fat_per_100g: 32,
+        carbohydrates_per_100g: 26,
+      },
+    },
+    {
+      text:"Posiłek 2: 12:00-15:00",
+        name: "Makaron z pesto pietruszkowym",
+        imgSrc: "images/foto_przepisy/obiad/makaron_z_pesto_pietruszkowym.png",
+        kcal: {
+          calories_per_100g: 474,
+          protein_per_100g: 42,
+          fat_per_100g: 16,
+          carbohydrates_per_100g: 39,
+        },
+      },
+      {
+        text:"Posiłek 3: 17:00-21:00",
+          name: "Koktajl czekoladowy",
+          imgSrc: "images/foto_przepisy/kolacja/koktajl_czekoladowy.png",
+          kcal: {
+            calories_per_100g: 551,
+            protein_per_100g: 26,
+            fat_per_100g: 18,
+            carbohydrates_per_100g: 76,
+          },
+        },
+  ],
+};
+const DayFive = {
+  data: "Piątek, 26.11",
+  meals: [
+    {
+      text:"Posiłek 1: 07:00-10:00",
+      name: "Tosty a la pizza",
+      imgSrc: "images/foto_przepisy/sniadanie/tosty_a_la_pizza.png",
+      kcal: {
+        calories_per_100g: 566,
+        protein_per_100g: 27,
+        fat_per_100g: 28,
+        carbohydrates_per_100g: 56,
+      },
+    },
+    {
+      text:"Posiłek 2: 12:00-15:00",
+        name: "Makaron z sosem bolońskim z tofu",
+        imgSrc: "images/foto_przepisy/obiad/makaron_z_sosem_bolonskim_z_tofu.png",
+        kcal: {
+          calories_per_100g: 248,
+          protein_per_100g: 13,
+          fat_per_100g: 11,
+          carbohydrates_per_100g: 24,
+        },
+      },
+      {
+        text:"Posiłek 3: 17:00-21:00",
+          name: "Owsianka z Inką",
+          imgSrc: "images/foto_przepisy/kolacja/owsianka_z_inka.png",
+          kcal: {
+            calories_per_100g: 171,
+            protein_per_100g: 10,
+            fat_per_100g: 6,
+            carbohydrates_per_100g: 22,
+          },
+        },
+  ],
+};
+const DaySix = {
+  data: "Sobota, 27.11",
+  meals: [
+    {
+      text:"Posiłek 1: 07:00-10:00",
+      name: "Jajecznica na parze",
+      imgSrc: "/images/foto_przepisy/sniadanie/jajecznica _na_parze.png",
+      kcal: {
+        calories_per_100g: 472,
+        protein_per_100g: 30,
+        fat_per_100g: 23,
+        carbohydrates_per_100g: 38,
+      },
+    },
+    {
+      text:"Posiłek 2: 12:00-15:00",
+        name: "Pinsa z szynką z kurczaka i suszonymi pomidorami",
+        imgSrc: "images/foto_przepisy/obiad/pinsa_z_szynka_z_kurczaka_i_suszonymi_pomidorami.png",
+        kcal: {
+          calories_per_100g: 298,
+          protein_per_100g: 17,
+          fat_per_100g: 11,
+          carbohydrates_per_100g: 32,
+        },
+      },
+      {
+        text:"Posiłek 3: 17:00-21:00",
+          name: "Ryżowe placuszki z owocami",
+          imgSrc: "images/foto_przepisy/kolacja/ryzowe_placuszki_z_owocami.png",
+          kcal: {
+            calories_per_100g: 261,
+            protein_per_100g: 8,
+            fat_per_100g: 5,
+            carbohydrates_per_100g: 48,
+          },
+        },
+  ],
+};
+const DaySeven = {
+  data: "Niedziela, 27.11",
+  meals: [
+    {
+      text:"Posiłek 1: 07:00-10:00",
+      name: "Kanapki z jajkiem na twardo i warzywami",
+      imgSrc: "/images/foto_przepisy/sniadanie/kanapki_z_jajkiem_na_twardo_i_warzywami.png",
+      kcal: {
+        calories_per_100g: 542,
+        protein_per_100g: 26,
+        fat_per_100g: 24,
+        carbohydrates_per_100g: 58,
+      },
+    },
+    {
+      text:"Posiłek 2: 12:00-15:00",
+        name: "Dorsz gotowany na parze z kuskusem",
+        imgSrc: "images/foto_przepisy/obiad/dorsz_gotowany_na_parze_z_kuskusem.png",
+        kcal: {
+          calories_per_100g: 212,
+          protein_per_100g: 13,
+          fat_per_100g: 7,
+          carbohydrates_per_100g: 24,
+        },
+      },
+      {
+        text:"Posiłek 3: 17:00-21:00",
+          name: "Sałatka z serem brie, winogronami, orzechami i grzankami",
+          imgSrc: "images/foto_przepisy/kolacja/salatka_z_serem_brie_winogronami_orzechami_i_grzankami.png",
+          kcal: {
+            calories_per_100g: 247,
+            protein_per_100g: 9,
+            fat_per_100g: 14,
+            carbohydrates_per_100g: 22,
+          },
+        },
+  ],
+};
+const testWeek = [DayOne, DayTwo, DayThree, DayFour, DayFive, DaySix, DaySeven];
 
 const WeeklyMenuPlan = () => {
   return (
     <div style={{display: "flex"}}>
       {testWeek.map((d) => (
-        <div>
+        <div style={styles.oneday}>
           
-            <div style={boxstyle} className="day-week">{d.data}</div>
+            <div style={styles.boxstyle} className="day-week">{d.data}</div>
 
             {d.meals.map((m) => (
-              <div style={container_plan}>
-                <img style={imgstyle} src={m.imgSrc} />
+              <div style={styles.container_plan}>
+                <img style={styles.imgstyle} src={m.imgSrc} />
                 <div>
 
                 <div >{m.text}</div>
@@ -39,7 +291,7 @@ const WeeklyMenuPlan = () => {
               </div>
             ))}
 
-            <div style={boxstyle} className="kcal">
+            <div style={styles.boxstyle} className="kcal">
               {d.meals.reduce((total, meal) => total + meal.kcal.calories_per_100g, 0)}
               Kcal.{" "}
 
@@ -58,19 +310,28 @@ const WeeklyMenuPlan = () => {
   );
 };
 
-const imgstyle={
+const styles = {
+
+  oneday:{
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px',
+    minWidth: '300px',
+    marginRight: '8px'
+  },
+imgstyle:{
     maxWidth: '100px',
     boxShadow: 'none'
-}
-const boxstyle={
+},
+boxstyle:{
     padding: '10px',
         border: '1px solid #e0e0e0',
         borderRadius: '10px',
         backgroundColor: '#f9f9f9',
         fontFamily: 'Arial, sans-serif',
         fontSize: '16px',
-}
-const container_plan={
+},
+container_plan:{
     display:"flex",
     flexDirection:'row',
      alignItems:'center',
@@ -79,5 +340,6 @@ const container_plan={
      borderRadius: '10px',
      backgroundColor: '#f9f9f9',
      fontFamily: 'Arial, sans-serif',
+}
 }
 export default WeeklyMenuPlan;
